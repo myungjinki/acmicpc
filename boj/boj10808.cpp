67834142deeab9b3eb1619fa63fe07f217e0e340
mki <mki@student.42seoul.kr>
2022-02-03 14:49:52 +0900

    refactor: boj10808.cpp
    Used iterator
    
    resolved: #45

--- boj/boj10808.cpp
+++ boj/boj10808.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
+int main(){
+  std::string S;
+  int array[26];
 
-int arr[26];
+  for (int i = 0; i < 26; i++) {
+    array[i] = 0;
+  }
 
-int main(void)
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    string str;
-    cin >> str;
-    for (int i = 0; i < str.size(); i++){
-        arr[str[i] - 'a']++;
-    }
-    for (int i = 0; i < 26; i++){
-        cout << arr[i] << " ";
-    }
+  std::cin >> S;
+  for (std::string::iterator iter = S.begin(); iter < S.end(); iter++) {
+    array[*iter - 'a']++;
+  }
+
+  for (int i = 0; i < 26; i++) {
+    std::cout << array[i] << " ";
+  }
 }
