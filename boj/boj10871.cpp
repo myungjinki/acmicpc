3bb3a2c181034b5fc8ecff9ebf19f6352bd9a481
mki <mki@student.42seoul.kr>
2022-02-03 12:13:21 +0900

    refactor: boj10871
    Added array and input first and print later
    
    resolved: #66

--- boj/boj10871.cpp
+++ boj/boj10871.cpp
@@ -2,17 +2,17 @@
 
 using namespace std;
 
+int N, X, A[10005];
 int main(void)
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int N, X;
     cin >> N >> X;
-    while (N--)
+    for (int i = 0; i < N; i++)
+        cin >> A[i];
+    for (int i = 0; i < N; i++)
     {
-        int A;
-        cin >> A;
-        if (A < X)
-            cout << A << " ";
+        if (A[i] < X)
+            cout << A[i] << " ";
     }
 }
