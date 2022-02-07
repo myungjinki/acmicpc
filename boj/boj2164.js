cfed7026afaabeb533d2bdbd53fa95195fb03d7e
mki <mki@student.42seoul.kr>
2022-02-07 15:33:51 +0900

    refactor: boj2164.js
    Short version
    
    resolved: #40

--- boj/boj2164.js
+++ boj/boj2164.js
@@ -1,10 +1,4 @@
-const fs = require("fs");
-const line = fs.readFileSync("/dev/stdin").toString();
-const num = Number(line);
+const num = Number(require("fs").readFileSync("/dev/stdin").toString());
 let max = 1;
-let answer = "";
-while (max < num) {
-    max *= 2;
-}
-answer += max - (max - num) * 2;
-console.log(answer);
+while (max < num) max *= 2;
+console.log(max - (max - num) * 2);
